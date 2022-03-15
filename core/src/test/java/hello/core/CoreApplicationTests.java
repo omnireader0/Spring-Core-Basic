9a5d074138540729d6873c2e8e9184291631e1ac
omnireader0 <omnireader0@gmail.com>
2022-03-15 13:59:08 +0900

    add : 싱글톤 컨테이너로 싱글톤 적용

--- core/src/test/java/hello/core/CoreApplicationTests.java
+++ core/src/test/java/hello/core/CoreApplicationTests.java
@@ -2,12 +2,23 @@ package hello.core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootTest
 class CoreApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void configurationDeep() {
+		//AppConfig도 스프링 빈으로 등록된다
+		ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+		AppConfig bean = ac.getBean(AppConfig.class);
+		//출력: bean = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$bd479d70
+		System.out.println("bean = " + bean.getClass());
 	}
 
 }
